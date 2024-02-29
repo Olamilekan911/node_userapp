cc9da3a08164e1c016cbfc97c6718997a6cabe08
Olamilekan <olamilekanayoola293@gmail.com>
2024-02-28 21:59:47 -1200

    index file

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
+
+//router
+
+//port
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+})
